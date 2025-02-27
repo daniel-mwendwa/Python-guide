24b3153b8d407c80c581d4eab18d5a96cc2e1f23
daniel <danielmwendwa234@gmail.com>
2025-02-27 10:17:36 +0300

    I Have updated Lists, Tuple and Dictionary

--- AWS_Re_start.py
+++ AWS_Re_start.py
@@ -46,6 +46,24 @@ print(myFruitList[1])
 print(myFruitList[2])
 myFruitList[2] = "orange"
 print(myFruitList)
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
 #Conditional
 age=19
 if age>18:
