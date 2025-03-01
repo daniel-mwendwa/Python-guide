57ad8b4e9916179bc48377d89d433a9b81c44315
daniel <danielmwendwa234@gmail.com>
2025-03-01 19:06:46 +0300

    I have made changes in composite data type

--- AWS_Re_start.py
+++ AWS_Re_start.py
@@ -124,6 +124,10 @@ with open('car_fleet.csv') as csvFile:
             myInventoryList.append(currentVehicle)  
             lineCount += 1  
     print(f'Processed {lineCount} lines.')
+    for myCarProperties in myInventoryList:
+        for key, value in myCarProperties.items():
+            print("{} : {}".format(key,value))
+            print("-----")
 next_session()
 #************************************************************************************
 #Conditional
