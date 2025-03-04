d59ba77d9ed7876efff57b8ccf6189d157872a5d
daniel <danielmwendwa234@gmail.com>
2025-03-04 10:36:47 +0300

    I have made some changes to add a Json file to help in understanding file handlers in pyhton

--- AWS_Re_start.py
+++ AWS_Re_start.py
@@ -260,3 +260,14 @@ def runCaesarCipherProgram():
 runCaesarCipherProgram()
 next_session()
 #************************************************************************************
+#creating File handlers and Modules  ---> Lab 126
+import json
+
+def readJsonFile(fileName):
+    data = ""
+    try:
+        with open(fileName) as json_file:
+            data = json.load(json_file)
+    except IOError:
+        print("Could not read file")
+    return data
